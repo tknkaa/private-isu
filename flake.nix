ba50d21cf6b9a94cf18fea2b82270f22584a5a78
tknkaa <tknkaa@users.noreply.github.com>
2026-07-19 16:00:46 +0900

    chore: set up nix flake and add apacheHttpd

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+
+      packages.${system} = {
+        hello = pkgs.hello;
+        default = self.packages.${system}.hello;
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.apacheHttpd
+        ];
+      };
+
+    };
+}
